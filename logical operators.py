77baa828e7395fd29b599accba9ab40e80a073b1
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-02-13 14:44:55 +0600

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,19 @@
+#NOT,AND,OR
+#not
+a=20
+b=10
+print("NOT=",not True)
+print("NOT=",not False)
+#always give opposite answer 
+
+#and 
+va1=True
+val2=False
+print("AND=" ,va1 and val2)
+
+#or
+print("OR=",va1 or val2)
+
+#using exprission
+print("And operator :", (a==b)and(a>b))
+print("OP operator :",(a==b)or(a>b))
